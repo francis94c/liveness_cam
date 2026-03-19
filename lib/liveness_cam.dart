1dedc3e11153b7fb1aeb5e760b05cd026860b3a2
The Full-Stack Jonin <francis94c@gmail.com>
2026-03-19 20:53:21 +0100

    feat: Old and new implementation

--- lib/liveness_cam.dart
+++ lib/liveness_cam.dart
@@ -1,13 +1,14 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:liveness_cam/camera_page_old.dart';
 
 import 'camera_page.dart';
 
 class LivenessCam {
   final _methodChannel = const MethodChannel('liveness_cam');
 
-  Future<File?> start(BuildContext context) async {
+  Future<File?> start(BuildContext context, {bool useOldCamera = false}) async {
     try {
       if (Platform.isAndroid) {
         final result = await _methodChannel.invokeMethod("start");
@@ -16,8 +17,11 @@ class LivenessCam {
         }
         return null;
       } else if (Platform.isIOS) {
-        final result = await Navigator.push(context,
-            MaterialPageRoute(builder: (context) => const CameraPage()));
+        final result = await Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    useOldCamera ? const CameraPageOld() : const CameraPage()));
         if (result != null) {
           return result as File;
         }
